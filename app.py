0e236b3fefdeea695b72c39f60de6a4a47c576b8
smandala1 <smandala1@student.gsu.edu>
2022-03-28 13:55:22 -0400

    Shubhada's commit

--- app.py
+++ app.py
@@ -4,24 +4,22 @@ from flask import Flask, render_template
 from nyt import get_article_data
 
 app = Flask(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
-    """ Returns root endpoint HTML """
+    """Returns root endpoint HTML"""
 
-    keyword_query = 'Apple' # Change it to something you're interested in!
+    keyword_query = "Headlines"  # Change it to something you're interested in!
     article_data = get_article_data(keyword_query)
 
     return render_template(
         "index.html",
         topic=keyword_query,
-        headlines=article_data['headlines'],
-        snippets=article_data['snippets'],
+        headlines=article_data["headlines"],
+        snippets=article_data["snippets"],
     )
 
-app.run(
-    host=os.getenv('IP', '0.0.0.0'),
-    port=int(os.getenv('PORT', 8080)),
-    debug=True
-)
+
+app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True)
